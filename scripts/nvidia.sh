d626ff690bdd9286928405045b505c43510c62b2
fei <fei@time>
2024-03-01 20:29:51 +0100

    print failed log

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -12,8 +12,14 @@ rpm-ostree install \
     libva-utils \
     vdpauinfo
 
+RELEASE="$(rpm -E '%fedora.%_arch')"
 KERNEL_VERSION="$(rpm -q kernel --queryformat '%{VERSION}-%{RELEASE}.%{ARCH}')"
+NVIDIA_PACKAGE_NAME="nvidia"
+NVIDIA_MAJOR_VERSION="550"
 
-akmods --force --kernels "${KERNEL_VERSION}" --kmod nvidia
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "${NVIDIA_PACKAGE_NAME}"
+
+modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/${NVIDIA_PACKAGE_NAME}/nvidia{,-drm,-modeset,-uvm}.ko.xz > /dev/null || \
+(cat /var/cache/akmods/${NVIDIA_PACKAGE_NAME}/${NVIDIA_AKMOD_VERSION}-for-${KERNEL_VERSION}.failed.log && exit 1)
 
 systemctl enable nvidia-{suspend,resume,hibernate}
