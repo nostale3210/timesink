1b78bf31ee5aebe488160991eac9cbb3a3d6bcf0
fei <fei@timesink>
2024-03-02 22:40:25 +0100

    nvidia 550 still needs patched xwayland

--- scripts/nvidia.sh
+++ scripts/nvidia.sh
@@ -2,6 +2,8 @@
 
 set -oue pipefail
 
+wget https://copr.fedorainfracloud.org/coprs/gloriouseggroll/nvidia-explicit-sync/repo/fedora-$(rpm -E %fedora)/gloriouseggroll-nvidia-explicit-sync-fedora-$(rpm -E %fedora).repo?arch=x86_64 -O /etc/yum.repos.d/_copr_gloriouseggroll-nvidia-explicit-sync.repo
+
 rpm-ostree install \
     binutils \
     akmods \
@@ -29,3 +31,8 @@ modinfo /usr/lib/modules/${KERNEL_VERSION}/extra/${NVIDIA_PACKAGE_NAME}/nvidia{,
 (cat /var/cache/akmods/${NVIDIA_PACKAGE_NAME}/${NVIDIA_AKMOD_VERSION}-for-${KERNEL_VERSION}.failed.log && exit 1)
 
 systemctl enable nvidia-{suspend,resume,hibernate}
+
+rpm-ostree override replace \
+    --experimental \
+    --from repo=copr:copr.fedorainfracloud.org:gloriouseggroll:nvidia-explicit-sync \
+    xorg-x11-server-Xwayland
