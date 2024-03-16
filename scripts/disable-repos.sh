6cdd94a74bc586a6c97ef281872d25608cc5c052
fei <fei@timesink>
2024-03-16 16:22:07 +0100

    experimental hyprland image

--- scripts/disable-repos.sh
+++ scripts/disable-repos.sh
@@ -4,6 +4,7 @@ set -oue pipefail
 
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_gloriouseggroll-nvidia-explicit-sync.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_kylegospo-system76-scheduler.repo
+sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/_copr_solopasha-hyprland.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/fedora-cisco-openh264.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-free-updates.repo
 sed -i 's@enabled=1@enabled=0@g' /etc/yum.repos.d/rpmfusion-free.repo
