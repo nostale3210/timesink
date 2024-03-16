e8ea0dfa99399ab92a4430d6236617acf1f217a6
fei <fei@timesink>
2024-03-16 22:08:13 +0100

    don't install dbus-python for now

--- scripts/hypr.sh
+++ scripts/hypr.sh
@@ -46,5 +46,13 @@ rpm-ostree install \
     thunar-archive-plugin \
     file-roller
 
+rpm-ostree install \
+    gstreamer1-plugin-libav \
+    gstreamer1-plugins-bad-free-extras \
+    gstreamer1-plugins-ugly \
+    gstreamer1-vaapi \
+    steam-devices \
+    ffmpeg
+
 systemctl enable bluetooth.service
 systemctl enable sddm.service
