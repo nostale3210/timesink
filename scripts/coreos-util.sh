3ad56020b8effbaec0ad972e321b9fa9a85e5a50
fei <fei@timesink>
2024-03-16 21:23:24 +0100

    2nd try

---
+++ scripts/coreos-util.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+rpm-ostree install \
+    wget
---
+++ scripts/hypr.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+set -oue pipefail
+
+rpm-ostree install \
+    hyprland \
+    hyprpaper \
+    hyprpicker \
+    hypridle \
+    hyprlock \
+    hyprcursor \
+    xdg-desktop-portal-hyprland \
+    xdg-desktop-portal-gtk \
+    wofi \
+    kitty \
+    mako \
+    pipewire \
+    wireplumber \
+    bluez \
+    bluez-tools \
+    blueberry \
+    pipewire-pulseaudio \
+    pipewire-alsa \
+    network-manager-applet \
+    NetworkManager \
+    qt5-wayland \
+    qt6-wayland \
+    qt5ct \
+    qt6ct \
+    waybar \
+    pamixer \
+    pavucontrol \
+    polkit-gnome \
+    qt \
+    wlogout \
+    xdg-user-dirs \
+    xdg-utils \
+    gnome-system-monitor \
+    gnome-disk-utility \
+    udiskie \
+    sddm \
+    ffmpegthumbnailer \
+    Thunar \
+    thunar-volman \
+    tumbler \
+    thunar-archive-plugin \
+    file-roller
+
+systemctl enable bluetooth.service
+systemctl enable sddm.service
