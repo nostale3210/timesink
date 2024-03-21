5d8a798e1d68921ded9ee4e196722e0c2f3058bf
fei <fei@timesink>
2024-03-21 20:16:44 +0100

    install wget

--- scripts/core.sh
+++ scripts/core.sh
@@ -3,6 +3,12 @@
 set -oue pipefail
 
 rpm-ostree install \
-    wget
+    ffmpeg \
+    firefox \
+    gstreamer1-plugin-libav \
+    gstreamer1-plugins-bad-free-extras \
+    gstreamer1-plugins-ugly \
+    gstreamer1-vaapi \
+    steam-devices
 
 systemctl disable zincati.service
