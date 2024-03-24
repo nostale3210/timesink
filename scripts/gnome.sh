6a3c45b58fc2ce82024e87bd2525c48ff95ba417
fei <fei@timesink>
2024-03-25 00:37:36 +0100

    iso action

--- scripts/gnome.sh
+++ scripts/gnome.sh
@@ -14,6 +14,9 @@ rpm-ostree override remove \
 
 pip install --prefix=/usr nautilus-open-any-terminal
 
+echo "com.raggesilver.BlackBox" >> /usr/share/timesink/flatpak/install
+echo "com.mattjakeman.ExtensionManager" >> /usr/share/timesink/flatpak/install
+
 sed -i 's@\[Desktop Entry\]@\[Desktop Entry\]\nNoDisplay=true@g' /usr/share/applications/org.gnome.Terminal.desktop
 
 glib-compile-schemas /usr/share/glib-2.0/schemas
